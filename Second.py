ed97c60f8da9775efe54f5c499afa31e518c2ba8
iainrwolfheart <iainrgorrie@gmail.com>
2019-05-24 10:37:29 +0100

    first

---
+++ Second.py
@@ -0,0 +1,5 @@
+physics=80
+maths=100
+chemistry=100
+total=physics+maths+chemistry
+print("The result is", total) 
---
+++ Second2.py
@@ -0,0 +1,11 @@
+physics=80
+maths=100
+chemistry=100
+total=physics+maths+chemistry
+per=total/450*100
+print("Physics:",physics)
+print("Maths:",maths)
+print("Chemistry:",chemistry)
+print("-------------------")
+print("Total marks:",total)
+print("Percentage:",per)
