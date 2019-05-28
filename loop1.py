1809cd1b33c624bf3ce2fcbd03de94473e553b8e
iainrwolfheart <iainrgorrie@gmail.com>
2019-05-28 16:44:21 +0100

    End of loop day

--- loop1.py
+++ loop1.py
@@ -1,7 +1,10 @@
-a=1
-while a<=10:
-	b=1
-	while b<=10:
-		print(b)
-		b+=1
-	a+=1
+msg=input("Write stuff here")
+find=input("Which word shall we look for?")
+f=0
+m=0
+while m<len(msg):
+	if msg[m:m+len(find)]==find:
+		f=f+1
+		m=m+len(find)-1
+	m=m+1
+print(f)
